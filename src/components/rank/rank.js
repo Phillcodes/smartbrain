2deea18e8ff4beedb74a55b40ea53c7f83ad8a57
Phillcodes <phillipjacob90@gmail.com>
2019-11-18 20:31:51 +0100

    all

---
+++ src/components/rank/rank.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+
+const Rank = () => {
+  return (
+  <div>
+      <div className='white f3'>
+      {'Phillip, your current rank is...'}
+      </div>
+      <div className='white f3'>
+      {'#5...'}
+      </div>
+  </div>
+  );
+}
+
+
+
+export default Rank;
